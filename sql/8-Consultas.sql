ea865d9d3320a368f1f740602c3f63af68c5c8e5
Diego Holanda <diegoholanda2008@hotmail.com>
2019-12-01 20:33:19 -0300

    Querry Questão 8 item B

--- sql/8-Consultas.sql
+++ sql/8-Consultas.sql
@@ -32,3 +32,27 @@ SELECT * FROM tb_albuns
 SELECT descricao
 FROM tb_albuns
 WHERE preco_de_compra > (SELECT AVG(preco_de_compra) FROM tb_albuns)
+
+-- Item C
+/*
+ALTER VIEW qtd_grav_play(id, nome, qtd)
+AS
+    SELECT  tg.cod_gravadora, tg.nome, COUNT(DISTINCT tp.cod_playlist) AS 'Quantidade de Playlists'
+    FROM tb_playlists tp INNER JOIN tb_faixas_playlists tfp ON tp.cod_playlist = tfp.cod_playlist
+        INNER JOIN tb_faixas tf ON tf.cod_faixa = tfp.cod_faixa
+        INNER JOIN tb_faixa_album tfa ON tf.cod_faixa = tfa.cod_faixa
+        INNER JOIN tb_albuns ta ON ta.cod_album = tfa.cod_album
+        INNER JOIN tb_gravadoras tg ON tg.cod_gravadora = ta.cod_gravadora
+        INNER JOIN tb_composta_por tbcp ON tbcp.cod_faixa = tf.cod_faixa
+        INNER JOIN tb_compositores tc ON tc.cod_compositor = tbcp.cod_compositor
+    WHERE tc.nome LIKE '%Dvorack%'
+    GROUP BY tg.cod_gravadora, tg.nome
+
+SELECT gp.nome
+FROM qtd_grav_play gp
+GROUP BY gp.nome, gp.qtd
+HAVING gp.qtd >= ALL (SELECT MAX(gp.qtd)
+                FROM qtd_grav_play gp
+                GROUP BY gp.nome, gp.qtd)
+
+*/
