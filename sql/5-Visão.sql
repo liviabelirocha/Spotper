cf9cf4f383e6228204c1ddbaa6bfcba05596d6d6
Diego Holanda <diegoholanda2008@hotmail.com>
2019-12-01 19:04:38 -0300

    Converting to materialized vision

--- sql/5-Visão.sql
+++ sql/5-Visão.sql
@@ -4,3 +4,14 @@ AS
     FROM tb_faixas_playlists fp INNER JOIN tb_playlists p
         ON p.cod_playlist = fp.cod_playlist
     GROUP BY p.nome
+
+/*
+CREATE VIEW v_num_albuns_playlist(cod_play,playlist, num_albuns) WITH SCHEMABINDING
+AS
+    SELECT p.cod_playlist cod, p.nome playlist, COUNT_BIG(*) num_albuns
+    FROM dbo.tb_faixas_playlists fp INNER JOIN dbo.tb_playlists p
+        ON p.cod_playlist = fp.cod_playlist
+    GROUP BY p.nome, p.cod_playlist
+
+CREATE UNIQUE CLUSTERED INDEX playlistAlbuns_index ON v_num_albuns_playlist(cod_play)
+*/
