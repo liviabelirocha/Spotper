8f8afa8fb61818e6e3baa87131cfaad6d2e963c8
Diego Holanda <diegoholanda2008@hotmail.com>
2019-12-02 10:35:43 -0300

    Trigger for value of album

--- sql/3-Triggers.sql
+++ sql/3-Triggers.sql
@@ -34,3 +34,25 @@ BEGIN
     ROLLBACK TRANSACTION
 END
 */
+
+/*
+Trigger Valor Maximo de Album
+CREATE VIEW albuns_DDD(cod_album, preco)
+AS 
+    SELECT DISTINCT ta.cod_album, preco_de_compra 
+    FROM tb_faixas tf INNER JOIN tb_faixa_album tfa ON tf.cod_faixa = tfa.cod_faixa
+        INNER JOIN tb_albuns ta ON ta.cod_album = tfa.cod_album
+    WHERE NOT EXISTS (SELECT tipo_gravacao 
+                        FROM tb_faixas tf2 INNER JOIN tb_faixa_album tfa ON tf2.cod_faixa = tfa.cod_faixa AND tf2.tipo_gravacao NOT LIKE '%DDD%'
+                        INNER JOIN tb_albuns ta2 ON ta2.cod_album = tfa.cod_album
+                        WHERE ta.cod_album = ta2.cod_album)
+                        
+CREATE TRIGGER album_preco_max ON tb_albuns
+FOR INSERT, UPDATE
+AS
+IF (SELECT 3*AVG(ad.preco) FROM albuns_DDD ad) < ANY (SELECT ab.preco_de_compra FROM tb_albuns ab)  
+BEGIN   
+    RAISERROR('Preço do album excedeu o valor máximo', 16, 1)
+    ROLLBACK TRANSACTION
+END
+*/
